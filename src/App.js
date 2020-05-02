204fe3ab1b24bd0827592cd181fb8ab8c286f418
Leon <leonndp@gmail.com>
2020-05-03 02:53:48 +0800

    Finished covid-tracker app

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 import { Cards, Chart, CountryPicker } from './components'
 import styles from './App.module.css'
-
 import { fetchData } from './api'
+
+import coronaImage from './images/image.png'
 
 class App extends React.Component {
     state = {
@@ -20,18 +21,19 @@ class App extends React.Component {
     handleCountryChange = async (country) => {
         const fetchedData = await fetchData(country)
 
-        console.log(fetchedData)
+        this.setState({ data: fetchedData, country: country })
         // set the state
     }
 
     render() {
-        const { data } = this.state
+        const { data, country } = this.state
 
         return (
             <div className={styles.container}>
+                <img className={styles.image} src={coronaImage} alt='COVID-19' />
                 <Cards data={data} />
-                <CountryPicker handleCountryChange={this.handleCountryChange} />
-                <Chart />
+                <CountryPicker handleCountryChange={this.handleCountryChange} country={country} />
+                <Chart data={data} country={country} />
             </div>
         )
 
